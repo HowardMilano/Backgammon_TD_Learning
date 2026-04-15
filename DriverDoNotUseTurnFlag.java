d81befa4bf124ffb5f6b1d4f4874ae018e23252d
Howard Milano <32373034+HowardMilano@users.noreply.github.com>
2026-04-14 19:31:46 -0700

    Enhance comments for move selection and game logic
    Add comments to clarify random move selection and game over conditions.

---
+++ DriverDoNotUseTurnFlag.java
@@ -0,0 +1,95 @@
+public class DriverDoNotUseTurnFlag {
+    private static double epsilon = 0.09; // How often should we pick a random move?
+
+    public static double[] getBoardNotation(GnuBoard gameBoard, Color player) {
+        if (player == Color.WHITE) {
+            return gameBoard.getBoardNotation(false);
+        }
+        else {
+            GnuBoard copy = new GnuBoard(gameBoard);
+            copy.flipPerspective();
+            return copy.getBoardNotation(false);
+        }
+    }
+
+    public static double getScore(double[] prediction) {
+        return prediction[0];
+    }
+
+    public static GnuMoves getBestMove(NeuralNetTwoHiddenTD nn,
+                                       int[] diceRole, 
+                                       GnuBoard gameBoard, 
+                                       Color player) {
+        Color otherPlayer = player == Color.WHITE ? Color.BLACK : Color.WHITE;
+        List<GnuMoves> legalMoves = gameBoard.getLegalMoves(diceRole);
+        if (legalMoves.isEmpty())
+            return null;
+
+        // Get the best move which results in the worst board for the opponent
+        Double bestScore = null;
+        GnuMoves bestMoves = null;
+        for (GnuMoves moves : legalMoves) {
+            GnuBoard freshBoard = new GnuBoard(gameBoard);
+            freshBoard.moveAndCheckGame(moves);
+            double[] prediction = nn.predict(getBoardNotation(freshBoard, otherPlayer));
+            // We want the worst score for the opponent
+            double score = getScore(prediction);
+            if (bestScore == null || score < bestScore) {
+                bestScore = score;
+                bestMoves = moves;
+            }
+        }
+
+        assert bestMoves != null;
+        // At times return a random move
+        if (Math.random() < epsilon && legalMoves.size() > 1) {
+            return legalMoves.get((int)(Math.random() * legalMoves.size()));
+        }
+        return bestMoves;
+    }
+
+    private double trainOnce(NeuralNetTwoHiddenTD nn) {
+        GnuBoard board = new GnuBoard();
+        board.setTurn(Math.random() >= 0.5 ? Color.WHITE : Color.BLACK);
+
+        nn.traceZeros();
+
+        while (true) {
+            int[] diceRole = board.roleDice();
+            // Do a move
+            Color player = board.getTurn();
+            Color otherPlayer = player == Color.WHITE ? Color.BLACK : Color.WHITE;
+            GnuBoard beforeMoveBoard = new GnuBoard(board);
+            GnuMoves bestMove = getBestMove(nn, diceRole, board, player);
+            board.moveAndCheckGame(bestMove);
+            if (board.isGameOver()) {
+                double afterGamePrediction[] = new double[1];
+                // Assumes a GnuBG style board, 0 - 23, 24 for bar, 25 for off
+                if (board.checkers[1][25] == 15)
+                {
+                    // Train the loser then the winner
+                    assert player == Color.BLACK;
+                    afterGamePrediction[0] = 0.0;
+                    nn.trainOne(getBoardNotation(beforeMoveBoard, otherPlayer), afterGamePrediction);
+                    afterGamePrediction[0] = 1.0;
+                    nn.trainOne(getBoardNotation(beforeMoveBoard, player), afterGamePrediction);
+                }
+                else {
+                    assert board.checkers[0][25] == 15;
+                    assert player == Color.WHITE;
+                    // Train the winner then the loser
+                    afterGamePrediction[0] = 1.0;
+                    nn.trainOne(getBoardNotation(beforeMoveBoard, player), afterGamePrediction);
+                    afterGamePrediction[0] = 0.0;
+                    nn.trainOne(getBoardNotation(beforeMoveBoard, otherPlayer), afterGamePrediction);
+                }
+                break;
+            } else {
+                double[] nextState = nn.predict(getBoardNotation(board, player));
+                nn.trainOne(getBoardNotation(beforeMoveBoard, player), nextState);
+            }
+        }
+
+        return 0;
+    }
+}
